e4d868f44bbb24ec68d900b1bd86db84dcb1ffc1
I. Dan Calinescu <idancalinescu@gmail.com>
2017-01-03 12:31:47 +0200

    added tests

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 import React         from 'react'
-import * as Config   from './lib/config'
-import * as Errors   from './lib/errors'
+import * as Config   from './src/config'
+import * as Errors   from './src/errors'
 
 // Give apps a starter for error handling
-export { default as AppError } from './lib/core/Error'
+export { default as AppError } from './src/core/Error'
 
 // Allow the app to define custom containers
-export { default as Container } from './lib/core/Container'
+export { default as Container } from './src/core/Container'
 
 // This is the main application wrapper and entry point
-export { default as AppContainer } from './lib/core/AppContainer'
+export { default as AppContainer } from './src/core/AppContainer'
 
 // Make the common containers available to the host app
-export * from './lib/data/containers'
+export * from './src/data/containers'
 
 // Expose operations to the host app
-export { default as RemoteOperation } from './lib/data/operations/RemoteOperation'
+export { default as RemoteOperation } from './src/data/operations/RemoteOperation'
 
 // Allow host apps to access common configurations
 export { Config }
 
 // Give apps access to the common cache
-export * from './lib/data/cache'
+export * from './src/data/cache'
